614b470e4c2db01336c70888a0784ba3695f3bed
babygoat <babygoat1124@gmail.com>
2019-01-17 17:52:30 +0800

    Add GetAll and GetById actions
    This patch adds the getAll and getById actions for /users route.

--- src/actions/User.ts
+++ src/actions/User.ts
@@ -2,6 +2,7 @@ import {Request, Response} from 'express';
 import {getManager} from 'typeorm';
 import {User} from '../entity/User';
 
+
 export async function userAdd(request: Request, response: Response) {
   const userRepository = getManager().getRepository(User);
 
@@ -10,4 +11,32 @@ export async function userAdd(request: Request, response: Response) {
   await userRepository.save(newUser);
 
   response.send(newUser);
+}
+
+export async function userGetAll(req: Request, res: Response) {
+  const userRepository = getManager().getRepository(User);
+  
+  const users: User[] = await userRepository.find();
+  
+  if (0 == users.length) {
+    res.status(204);
+    res.end();
+    return;
+  }
+
+  res.send(users);
+}
+
+export async function userGetById(req: Request, res: Response) {
+  const userRepository = getManager().getRepository(User);
+  
+  const user: User = await userRepository.findOne(req.params.id);
+
+  if (!user) {
+    res.status(404);
+    res.end();
+    return;
+  }
+
+  res.send(user)
 }
--- src/routes/User.ts
+++ src/routes/User.ts
@@ -1,9 +1,19 @@
-import {userAdd} from '../actions/User';
+import {userAdd, userGetAll, userGetById} from '../actions/User';
 
 export const userRoutes = [
   {
     path: '/users',
     method: 'post',
     action: userAdd,
+  },
+  {
+    path: '/users',
+    method: 'get',
+    action: userGetAll,
+  },
+  {
+    path: '/users/:id',
+    method: 'get',
+    action: userGetById
   },
 ]
